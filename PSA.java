4fb4ebad48636fd45420416641014de4e81c70a5
ryu225 <143040279+ryu225@users.noreply.github.com>
2024-10-14 18:08:03 +0300

    code

--- PSA.java
+++ PSA.java
@@ -1,1 +1,72 @@
-dndhh
+dndhhpackage OS;
+//PSA process stratigy algorithem 
+public class PSA {
+    private String ProcessID;
+    private int arrivalTime;
+    private int brustTime;
+    private int startTime;
+    private int termintionTime;
+    private int waitTime;
+    private int responseTime;
+    private int turnaroundTime;
+
+    public PSA (String ProcessID,int arrivalTime,int brustTime){
+    this.ProcessID=ProcessID;
+    this.arrivalTime=arrivalTime;
+    this.brustTime=brustTime;
+    this.startTime=0;
+    this.termintionTime=0;
+    this.turnaroundTime=0;
+    this.waitTime=0;
+    this.responseTime=0;
+    }
+    
+    public String getProcessID(){
+        return ProcessID;
+    }
+    public void setProcessID(String id){
+        ProcessID=id;
+    }
+    public int getbrustTime(){
+        return brustTime;
+    }
+    public void setbrustTime(int b){
+        brustTime=b;
+    }
+    public int getstartTime(){
+        return startTime;
+    }
+    public void setstartTime(int s){
+        startTime=s;
+    }
+    public int getTermintionTime(){
+        return termintionTime;
+    }
+    public void setTermintionTime(int t){
+        termintionTime=t;
+    }
+    public int getwaitTime(){
+        return waitTime;
+    }
+    public void setwaitTime(int w){
+        waitTime=w;
+    }
+    public int getresponseTime(){
+        return responseTime;
+    }
+    public void setresponseTime(int r){
+        responseTime=r;
+    }
+    public int getTurnaroundTime(){
+        return turnaroundTime;
+    }
+    public void setTurnaroundTime(int t){
+        turnaroundTime=t;
+    }
+    public int getArrivalTime(){
+        return arrivalTime;
+    }
+    public void setarrivalTime(int a){
+        arrivalTime=a;
+    }
+}
--- RR.java
+++ RR.java
@@ -1,1 +1,61 @@
 dd
+package OS;
+import java.io.*;
+import java.util.*;
+
+public class RR {
+
+    static Scanner in=new Scanner(System.in);
+    static List<PSA> q1= new ArrayList<>();
+    static List<PSA> q2= new ArrayList<>();
+    static List<PSA> gantChart= new ArrayList<>();
+    static int ProcessID=0;
+    static int currentTime=0;
+    static boolean terminated =true;
+
+    public static void main(String[] args){
+        int NOProceses=0;
+        try{
+
+            System.out.println("enter number of processes: ");
+            NOProceses=in.nextInt();
+            if (NOProceses<=0)
+                throw new IllegalArgumentException(" Number of process can not be zero or negative.");
+             } catch(InputMismatchException e){System.out.println("Input mismatch! please only enter integers./n try again: ");
+                in.next();
+            }catch(IllegalArgumentException e){
+                System.out.println("Invalid input: "+e.getMessage());
+                while(NOProceses<0){
+                    System.out.println("Illegal Number of processes, reEnter: ");
+                    NOProceses=in.nextInt();}
+            }
+            for (int i =0; i<NOProceses; i++){
+                System.out.println("\nEnter Process "+(ProcessID+1)+" details:");
+                try{
+                    System.out.println("Enter process arrival time: ");
+                    int arrivalTime=in.nextInt();
+                    while (arrivalTime < 0) {
+                        System.out.print("Illegal number of arrivalTime,reEnter:");
+                        arrivalTime = in.nextInt();
+                    }
+                    System.out.println("Enter process brust time: ");
+                    int brustTime=in.nextInt();
+                    while (brustTime < 0) {
+                        System.out.print("Illegal number of brust Time,reEnter:");
+                        brustTime = in.nextInt();
+                    }
+
+                    PSA process= new PSA("P"+ ++ProcessID, arrivalTime, brustTime);
+                    q1.add(process);
+                }catch (InputMismatchException e){
+                    System.out.println("Some of your inputs are mismatched! Only integers allowed. \nPlease enter again: ");
+				    in.next();
+				    i--;} 
+            }
+            System.out.println();
+    }
+    public RR (){
+        PSA curProcess= q1.remove(0);
+        
+    }
+}
